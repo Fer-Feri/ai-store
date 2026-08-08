b45055c24b80b0bdf101d4e5d4ec26a3e68dcf93
aloy33 <fsrshad.bahari.33@gmail.com>
2026-08-08 14:45:07 +0330

    add show more and show less btn in product grid

--- app/page.tsx
+++ app/page.tsx
@@ -3,35 +3,74 @@ import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
 import ProductGrid from "@/components/products/ProductGrid";
 import { Product } from "@/types";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+const LIMIT = 8;
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        if (!response.ok) throw new Error("خطا در دریافت محصولات");
-        const data: Product[] = await response.json();
-        setProducts(data);
-      } catch (error) {
-        setError("محصولات بارگذاری نشدند. دوباره تلاش کنید");
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
+  // const [loadingMore, setLoadingMore] = useState(false);
+  const [offset, setOffset] = useState(0); // offset → از کجا شروع کنیم؟ اول: 0، بعد: 8، بعد: 16...
+  const [hasMore, sethasMore] = useState(true); // hasMore → آیا محصول بیشتری وجود داره؟
+
+  // useCallback → این تابع رو cache میکنه، هر render دوباره ساخته نمیشه
+  const fetchProducts = useCallback(async (currentOffset: number) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        `https://fakestoreapi.com/products?limit=${LIMIT + currentOffset}`,
+      );
+      if (!response.ok) throw new Error("خطا در دریافت محصولات");
+
+      const data: Product[] = await response.json();
+
+      setProducts(data);
+      sethasMore(data.length >= LIMIT + currentOffset); // اگه تعداد برگشتی کمتر از LIMIT بود یعنی دیگه محصولی نداره
+    } catch (error: unknown) {
+      if (error instanceof Error) setError(error.message);
+    } finally {
+      setLoading(false);
     }
-    fetchProducts();
   }, []);
+
+  // اولین بار که صفحه لود میشه
+  useEffect(() => {
+    fetchProducts(0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleLoadMore = () => {
+    const newOffset = offset + LIMIT;
+    setOffset(newOffset);
+    fetchProducts(newOffset);
+  };
+
+  const handleLoadLess = () => {
+    if (offset <= 0) return;
+    const newOffset = offset - LIMIT;
+    setOffset(newOffset);
+    fetchProducts(newOffset);
+  };
+
   return (
     <main className="flex min-h-screen flex-col gap-4 p-10">
       <Header />
       <div className="mt-2 flex flex-col flex-wrap gap-6 md:flex-row">
         <Sidebar />
-        <ProductGrid products={products} loading={loading} error={error} />
+        <ProductGrid
+          products={products}
+          loading={loading}
+          error={error}
+          hasMore={hasMore}
+          onLoadMore={handleLoadMore}
+          onLoadLess={handleLoadLess}
+          canShowLess={offset > 0}
+        />
       </div>
     </main>
   );
--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -5,24 +5,34 @@ type ProductsGridProps = {
   products: Product[];
   loading: boolean;
   error: string | null;
+  hasMore: boolean;
+  canShowLess: boolean;
+  onLoadMore: () => void;
+  onLoadLess: () => void;
 };
 
 export default function ProductGrid({
   products,
   loading,
   error,
+  hasMore,
+  canShowLess,
+  onLoadMore,
+  onLoadLess,
 }: ProductsGridProps) {
   if (loading) {
-    <main className="flex-1 p-6">
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {Array.from({ length: 8 }).map((_, i) => (
-          <div
-            key={i}
-            className="h-64 animate-pulse rounded-xl bg-gray-200"
-          ></div>
-        ))}
-      </div>
-    </main>;
+    return (
+      <main className="flex-1 p-6">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {Array.from({ length: 8 }).map((_, i) => (
+            <div
+              key={i}
+              className="h-64 animate-pulse rounded-xl bg-gray-200"
+            ></div>
+          ))}
+        </div>
+      </main>
+    );
   }
 
   if (error) {
@@ -38,6 +48,29 @@ export default function ProductGrid({
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
+      </div>
+
+      <div className="flex items-center justify-center gap-3">
+        {hasMore && (
+          <div className="mt-8 flex justify-center">
+            <button
+              onClick={onLoadMore}
+              className="rounded-xl bg-blue-600 px-6 py-2 text-white hover:bg-blue-700"
+            >
+              Load More
+            </button>
+          </div>
+        )}
+        {canShowLess && (
+          <div className="mt-8 flex justify-center">
+            <button
+              onClick={onLoadLess}
+              className="rounded-xl bg-red-600 px-6 py-2 text-white transition-all hover:bg-red-700"
+            >
+              Show Less
+            </button>
+          </div>
+        )}
       </div>
     </main>
   );
